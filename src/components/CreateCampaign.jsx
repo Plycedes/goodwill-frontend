419c23aa6a1dd744ee3d6ccea12f86fc84e4f819
Plycedes <abhaypratapyadav340@gmail.com>
2024-08-24 23:08:52 +0530

    Craete Campaign

--- src/components/CreateCampaign.jsx
+++ src/components/CreateCampaign.jsx
@@ -1,7 +1,99 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { setCampaigns } from "../utils/api";
+import { CustomButton } from "../components";
 function CreateCampaign() {
-    return <div>CreateCampaign</div>;
+    const [owner, setOwner] = useState("");
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+    const [target, setTarget] = useState("");
+    const [image, setImage] = useState("");
+    return (
+        <div>
+            <div className="h-screen flex justify-center items-center mt-20 mb-20 ">
+                <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                    <div class="mb-4">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" for="name">
+                            Name of Student
+                        </label>
+                        <input
+                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            id="name"
+                            type="text"
+                            placeholder="Name of Student"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                        />
+                    </div>
+                    <div class="mb-4">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" for="rollno">
+                            Roll Number
+                        </label>
+                        <input
+                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            id="rollno"
+                            type="text"
+                            placeholder="Roll Number"
+                            value={rollno}
+                            onChange={(e) => setRollno(e.target.value)}
+                        />
+                    </div>
+                    <div class="mb-4">
+                        <label
+                            className="block text-gray-700 text-sm font-bold mb-2"
+                            for="mobileno"
+                        >
+                            Mobile Number
+                        </label>
+                        <input
+                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            id="mobileno"
+                            type="text"
+                            placeholder="Mobile Number"
+                            value={mobileno}
+                            onChange={(e) => setMobileno(e.target.value)}
+                        />
+                    </div>
+                    <div class="mb-4">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" for="math">
+                            Math
+                        </label>
+                        <input
+                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            id="math"
+                            type="text"
+                            placeholder="Math"
+                            value={math}
+                            onChange={(e) => setMath(e.target.value)}
+                        />
+                    </div>
+                    <div class="mb-4">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" for="physics">
+                            Physics
+                        </label>
+                        <input
+                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            id="physics"
+                            type="text"
+                            placeholder="Physics"
+                            value={physics}
+                            onChange={(e) => setPhysics(e.target.value)}
+                        />
+                    </div>
+
+                    <div className="flex items-center justify-between">
+                        <CustomButton
+                            btnType="button"
+                            title={"Create"}
+                            styles={""}
+                            handleClick={() => {
+                                setCampaigns();
+                            }}
+                        />
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
 }
 
 export default CreateCampaign;
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,14 @@
 import { ethers } from "ethers";
 import { abi, contractAddress } from "../constants/contract.js";
 
-let signer, provider;
+let signer, provider, owner, title, description, target, image;
+
+owner = "0xF416A40EdF205c563E7B12Fdd3a21710c55CfDA3";
+title = "Startup 1";
+description = "Need funding for a startup";
+target = ethers.parseEther("1.54234");
+image =
+    "https://img.jagranjosh.com/images/2022/August/1082022/what-is-a-start-up-types-funding-compressed.webp";
 
 export const connect = async () => {
     if (window.ethereum !== null) {
@@ -23,7 +30,21 @@ export const getCampaigns = async () => {
         const contract = new ethers.Contract(contractAddress, abi, signer);
         console.log("Mining");
         const tx = await contract.getCampaigns();
-        console.log(tx);
+        console.log(tx[0].title);
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export const setCampaigns = async () => {
+    try {
+        provider = new ethers.BrowserProvider(window.ethereum);
+        signer = await provider.getSigner();
+        const contract = new ethers.Contract(contractAddress, abi, signer);
+        console.log("Mining");
+        const tx = await contract.createCampaign(owner, title, description, target, image);
+        await tx.wait();
+        console.log("Complete");
     } catch (error) {
         console.log(error);
     }
