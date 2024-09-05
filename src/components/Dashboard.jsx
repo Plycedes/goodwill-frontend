2ebd5f983071eca2a08749170dc3983f94b33c9f
Plycedes <abhaypratapyadav340@gmail.com>
2024-09-05 12:20:58 +0530

    Changes

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { getCampaigns } from "../utils/api.js";
-import { CampaignCard } from "../components";
+import { CampaignCard, Loader } from "../components";
 
 function Dashboard() {
     const [campaigns, setCampaigns] = useState([]);
@@ -16,13 +16,17 @@ function Dashboard() {
         <div className="m-5">
             <h1 className="text-3xl font-bold text-orange-600">Campaigns</h1>
             <div className="mt-5 flex justify-center">
-                <ul className="grid grid-cols-3">
-                    {campaigns.map((campaign, i) => (
-                        <li key={campaign.title} className="mr-10 mb-10">
-                            <CampaignCard campaign={campaign} index={i} />
-                        </li>
-                    ))}
-                </ul>
+                {window.ethereum ? (
+                    <ul className="grid grid-cols-3">
+                        {campaigns.map((campaign, i) => (
+                            <li key={campaign.title} className="mr-10 mb-10">
+                                <CampaignCard campaign={campaign} index={i} />
+                            </li>
+                        ))}
+                    </ul>
+                ) : (
+                    <Loader text="Install Metamask to continue" loader={false} />
+                )}
             </div>
         </div>
     );
